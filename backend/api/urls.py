61aeded19c957949b420e4eb0eb4a145fa3509c9
Руслан Демидов <demidovbox@mail.ru>
2023-11-28 19:40:23 +0300

    Add serializers, views, routers for Tag,ingredient

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path, include
+from rest_framework import routers
+
+from .views import TagViewSet, IngredientViewSet
+
+router_v1 = routers.DefaultRouter()
+router_v1.register(r'tags', TagViewSet, basename='tags')
+router_v1.register(r'ingredients', IngredientViewSet, basename='ingredients')
 
 urlpatterns = [
+    path('', include(router_v1.urls)),
     path('', include('djoser.urls')),
 ]
