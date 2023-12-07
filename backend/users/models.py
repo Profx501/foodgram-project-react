969f54d5a9d4a9b6902f7981758e8f0b7dec55e2
Руслан Демидов <demidovbox@mail.ru>
2023-12-07 20:42:20 +0300

    Fix models.py

--- backend/users/models.py
+++ backend/users/models.py
@@ -18,6 +18,7 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+        ordering = ('first_name',)
 
     def __str__(self):
         return f'Пользователь: {self.email}'
